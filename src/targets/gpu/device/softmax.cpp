6588a7422e34ee6082e870fc4498491614250585
Shucai Xiao <shucai@gmail.com>
2020-12-06 09:56:13 -0600

    implementation of avoiding calculating index from multi-idx

--- src/targets/gpu/device/softmax.cpp
+++ src/targets/gpu/device/softmax.cpp
@@ -15,38 +15,84 @@ namespace device {
 void softmax(hipStream_t stream, const argument& result, const argument& arg, int64_t axis)
 {
     auto batch_lens          = result.get_shape().lens();
-    index_int batch_item_num = batch_lens[axis];
+    const index_int batch_item_num = batch_lens[axis];
     batch_lens[axis]         = 1;
     migraphx::shape batch_shape{result.get_shape().type(), batch_lens};
 
     hip_visit_all(result, arg, batch_shape)([&](auto output, auto input, auto batch) {
-        const index_int max_block_size = 256;
+        const index_int max_block_size = 64;
         const index_int block_size     = compute_block_size(batch_item_num, max_block_size);
+        const auto* input_ptr = device_cast(input.data());
+        auto* output_ptr        = device_cast(output.data());
         gs_launch(stream,
                   batch_shape.elements() * block_size,
                   block_size)([=](auto i, auto idx) __device__ {
-            auto data_idx = batch.multi(i / block_size);
+            auto offset = batch_item_num * idx.group;
             using type    = device_type<std::remove_cv_t<typename decltype(input)::value_type>>;
-            type init     = lowest();
+            MIGRAPHX_DEVICE_SHARED type buffer[384];
+            for (int tidx = idx.local; tidx < batch_item_num; tidx += block_size)
+            {
+                buffer[tidx] = input_ptr[offset + tidx];
+            }
 
-            auto batch_max = block_reduce<max_block_size>(
-                idx, max{}, init, batch_item_num, [&](auto j) __device__ {
-                    data_idx[axis] = j;
-                    return input[data_idx];
-                });
+            // block_reduce to compute max
+            int stride = batch_item_num / 2;
+            while (stride > 0)
+            {
+                for (int tidx = idx.local; tidx < stride; ++tidx)
+                {
+                    buffer[tidx] = (buffer[tidx] < buffer[tidx + stride]) ? buffer[tidx + stride] : buffer[tidx];
+                }
+                __syncthreads();
+                stride = stride / 2;
+            }
 
-            auto batch_sum =
-                block_reduce<max_block_size>(idx, sum{}, 0, batch_item_num, [&](auto j) __device__ {
-                    data_idx[axis] = j;
-                    auto val       = input[data_idx] - batch_max;
-                    return ::exp(to_hip_type(val));
-                });
+            type max_val = buffer[0];
+            for (int tidx = idx.local; tidx < batch_item_num; tidx += block_size)
+            {
+                buffer[tidx] = ::exp(to_hip_type(input_ptr[tidx] - max_val));
+            }
 
-            idx.local_stride(batch_item_num, [&](auto j) __device__ {
-                data_idx[axis]   = j;
-                auto val         = input[data_idx] - batch_max;
-                output[data_idx] = ::exp(to_hip_type(val)) / batch_sum;
-            });
+            stride = batch_item_num / 2;
+            while (stride > 0)
+            {
+                for (int tidx = idx.local; tidx < stride; ++tidx)
+                {
+                    buffer[tidx] = buffer[tidx] + buffer[tidx + stride];
+                }
+                __syncthreads();
+                stride = stride / 2;
+            }
+
+            type sum_val = buffer[0];
+
+            for (int tidx = idx.local; tidx < batch_item_num; tidx += block_size)
+            {
+                output_ptr[offset + tidx] = ::exp(to_hip_type(input_ptr[tidx] - max_val)) / sum_val;
+            }
+
+            // auto data_idx = batch.multi(i / block_size);
+            // using type    = device_type<std::remove_cv_t<typename decltype(input)::value_type>>;
+            // type init     = lowest();
+
+            // auto batch_max = block_reduce<max_block_size>(
+            //     idx, max{}, init, batch_item_num, [&](auto j) __device__ {
+            //         data_idx[axis] = j;
+            //         return input[data_idx];
+            //     });
+
+            // auto batch_sum =
+            //     block_reduce<max_block_size>(idx, sum{}, 0, batch_item_num, [&](auto j) __device__ {
+            //         data_idx[axis] = j;
+            //         auto val       = input[data_idx] - batch_max;
+            //         return ::exp(to_hip_type(val));
+            //     });
+
+            // idx.local_stride(batch_item_num, [&](auto j) __device__ {
+            //     data_idx[axis]   = j;
+            //     auto val         = input[data_idx] - batch_max;
+            //     output[data_idx] = ::exp(to_hip_type(val)) / batch_sum;
+            // });
         });
     });
 }
