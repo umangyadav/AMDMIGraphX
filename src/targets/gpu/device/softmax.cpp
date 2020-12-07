81f89e373e7f86591c7fb60d7147da8499a2b390
Shucai Xiao <shucai@gmail.com>
2020-12-06 22:16:03 -0600

    code backup

--- src/targets/gpu/device/softmax.cpp
+++ src/targets/gpu/device/softmax.cpp
@@ -20,7 +20,7 @@ void softmax(hipStream_t stream, const argument& result, const argument& arg, in
     migraphx::shape batch_shape{result.get_shape().type(), batch_lens};
 
     hip_visit_all(result, arg, batch_shape)([&](auto output, auto input, auto batch) {
-        const index_int max_block_size = 64;
+        const index_int max_block_size = 128;
         const index_int block_size     = compute_block_size(batch_item_num, max_block_size);
         const auto* input_ptr = device_cast(input.data());
         auto* output_ptr        = device_cast(output.data());
@@ -29,17 +29,17 @@ void softmax(hipStream_t stream, const argument& result, const argument& arg, in
                   block_size)([=](auto i, auto idx) __device__ {
             auto offset = batch_item_num * idx.group;
             using type    = device_type<std::remove_cv_t<typename decltype(input)::value_type>>;
-            MIGRAPHX_DEVICE_SHARED type buffer[64];
+            // MIGRAPHX_DEVICE_SHARED type buffer[128];
+            // type in_val[3];
+            // in_val[0] = input_ptr[idx.local + offset];
+            // in_val[1] = input_ptr[idx.local + offset + 128];
+            // in_val[2] = input_ptr[idx.local + offset + 256];
 
-            // buffer[idx.local] = lowest();
-            // for (int tidx = idx.local; tidx < batch_item_num; tidx += block_size)
-            // {
-            //     auto val = input_ptr[offset + tidx];
-            //     buffer[idx.local] = val > buffer[idx.local] ? val : buffer[idx.local];
-            // }
+            // buffer[idx.local] = (in_val[0] > in_val[1]) ? in_val[0] : in_val[1];
+            // buffer[idx.local] = (in_val[2] > buffer[idx.local]) ? in_val[2] : buffer[idx.local];
 
             // // block_reduce to compute max
-            // int stride = 32;
+            // int stride = 64;
             // while (stride > 0)
             // {
             //     for (int tidx = idx.local; tidx < stride; ++tidx)
@@ -51,52 +51,50 @@ void softmax(hipStream_t stream, const argument& result, const argument& arg, in
             // }
 
             // type max_val = buffer[0];
-            buffer[idx.local] = 0;
-            for (int tidx = idx.local; tidx < batch_item_num; tidx += block_size)
-            {
-                buffer[idx.local] += ::exp(to_hip_type(input_ptr[tidx]));
-            }
+            // __syncthreads();
 
-            int stride = 32;
-            while (stride > 0)
-            {
-                for (int tidx = idx.local; tidx < stride; ++tidx)
-                {
-                    buffer[tidx] = buffer[tidx] + buffer[tidx + stride];
-                }
-                __syncthreads();
-                stride = stride / 2;
-            }
+            // buffer[idx.local] = ::exp(to_hip_type((in_val[0] - max_val) * (in_val[1] - max_val) * (in_val[2] - max_val)));
 
-            type sum_val = buffer[0];
+            // stride = 64;
+            // while (stride > 0)
+            // {
+            //     for (int tidx = idx.local; tidx < stride; ++tidx)
+            //     {
+            //         buffer[tidx] = buffer[tidx] + buffer[tidx + stride];
+            //     }
+            //     __syncthreads();
+            //     stride = stride / 2;
+            // }
 
-            for (int tidx = idx.local; tidx < batch_item_num; tidx += block_size)
-            {
-                output_ptr[offset + tidx] = ::exp(to_hip_type(input_ptr[tidx])) / sum_val;
-            }
+            // type sum_val = buffer[0];
+            // __syncthreads();
 
-            // auto data_idx = batch.multi(i / block_size);
-            // using type    = device_type<std::remove_cv_t<typename decltype(input)::value_type>>;
-            // type init     = lowest();
+            // output_ptr[idx.local + offset] = ::exp(to_hip_type(in_val[0] - max_val)) / sum_val;
+            // output_ptr[idx.local + offset + 128] = ::exp(to_hip_type(in_val[1] - max_val)) / sum_val;
+            // output_ptr[idx.local + offset + 256] = ::exp(to_hip_type(in_val[2] - max_val)) / sum_val;
 
-            // auto batch_max = block_reduce<max_block_size>(
-            //     idx, max{}, init, batch_item_num, [&](auto j) __device__ {
-            //         data_idx[axis] = j;
-            //         return input[data_idx];
-            //     });
+            auto data_idx = batch.multi(i / block_size);
+            using type    = device_type<std::remove_cv_t<typename decltype(input)::value_type>>;
+            type init     = lowest();
 
-            // auto batch_sum =
-            //     block_reduce<max_block_size>(idx, sum{}, 0, batch_item_num, [&](auto j) __device__ {
-            //         data_idx[axis] = j;
-            //         auto val       = input[data_idx] - batch_max;
-            //         return ::exp(to_hip_type(val));
-            //     });
+            auto batch_max = block_reduce<max_block_size>(
+                idx, max{}, init, batch_item_num, [&](auto j) __device__ {
+                    data_idx[axis] = j;
+                    return input[data_idx];
+                });
 
-            // idx.local_stride(batch_item_num, [&](auto j) __device__ {
-            //     data_idx[axis]   = j;
-            //     auto val         = input[data_idx] - batch_max;
-            //     output[data_idx] = ::exp(to_hip_type(val)) / batch_sum;
-            // });
+            auto batch_sum =
+                block_reduce<max_block_size>(idx, sum{}, 0, batch_item_num, [&](auto j) __device__ {
+                    data_idx[axis] = j;
+                    auto val       = input[data_idx] - batch_max;
+                    return ::exp(to_hip_type(val));
+                });
+
+            idx.local_stride(batch_item_num, [&](auto j) __device__ {
+                data_idx[axis]   = j;
+                auto val         = input[data_idx] - batch_max;
+                output[data_idx] = ::exp(to_hip_type(val)) / batch_sum;
+            });
         });
     });
 }
