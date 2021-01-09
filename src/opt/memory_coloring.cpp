a38148911ad026cfc4e3a269e549c3da03246160
Paul <pfultz2@yahoo.com>
2021-01-08 18:23:25 -0600

    Add allocation_segment

--- src/opt/memory_coloring.cpp
+++ src/opt/memory_coloring.cpp
@@ -4,6 +4,7 @@
 #include <migraphx/instruction.hpp>
 #include <migraphx/iterator_for.hpp>
 #include <migraphx/functional.hpp>
+#include <migraphx/algorithm.hpp>
 #include <migraphx/ranges.hpp>
 #include <migraphx/stringutils.hpp>
 #include <unordered_set>
@@ -77,6 +78,133 @@ instruction_set_map build_conflict_table(const module& m, std::string allocation
     }));
     return conflict_table;
 }
+
+// Check if intervals overlap
+bool is_overlap(std::pair<std::size_t, std::size_t> x, std::pair<std::size_t, std::size_t> y)
+{
+    return std::max(x.first, y.first) < std::min(x.second, y.second);
+}
+
+const std::size_t alignment = 32;
+
+struct allocation_segment
+{
+    using segment = std::pair<std::size_t, std::size_t>;
+    std::unordered_map<instruction_ref, segment> ins2segment;
+
+    const segment* add_segment(instruction_ref ins, segment s)
+    {
+        // this->remove(ins);
+        return &(ins2segment[ins] = s);
+    }
+
+    const segment* get_segment(instruction_ref ins) const
+    {
+        auto it = ins2segment.find(ins);
+        if (it == ins2segment.end())
+            return nullptr;
+        return &it->second;
+    }
+
+    // Remove segment for an instruction
+    void remove(instruction_ref ins)
+    {
+        auto it = ins2segment.find(ins);
+        if(it != ins2segment.end())
+        {
+            ins2segment.erase(it);
+        }
+    }
+
+    static bool overlaps(const std::set<segment>& segments, const segment& s)
+    {
+        auto it = std::find_if(segments.begin(), segments.end(), [&](auto&& t) {
+            return is_overlap(s, t);
+        });
+        return it != segments.end();
+    }
+
+    static segment next_segment(std::set<segment>& segments, instruction_ref ins)
+    {
+        assert(ins->get_shape().bytes() > 0);
+        auto n = 1 + (ins->get_shape().bytes() - 1) / alignment;
+        assert(n > 0);
+        auto start = 0;
+        // Insert at end if it can fit at the begining
+        if (segments.empty() or segments.begin()->first <= n)
+        {
+            auto it =
+                std::adjacent_find(segments.begin(), segments.end(), [&](segment x, segment y) {
+                    if (x.second >= y.first)
+                        return false;
+                    auto k = y.first - x.second;
+                    return (k >= n);
+                });
+            if (it == segments.end() and not segments.empty())
+                it--;
+            if (it != segments.end())
+                start = it->second;
+        }
+        auto s = segment{start, start+n};
+        assert(not overlaps(segments, s));
+        segments.insert(s);
+        return s;
+    }
+
+    // Build the allocation_color class from the conflict_table
+    static allocation_segment build(const instruction_set_map& conflict_table)
+    {
+        allocation_segment as{};
+        std::vector<instruction_ref> conflict_queue;
+        // Add all allocations to the conflict_queue
+        std::transform(conflict_table.begin(),
+                       conflict_table.end(),
+                       std::back_inserter(conflict_queue),
+                       [](auto&& pp) { return pp.first; });
+
+        // Sort the conflict queue so we process the allocation with the least
+        // number of adjacent allocations first
+        std::sort(conflict_queue.begin(), conflict_queue.end(), [&](auto x, auto y) {
+            return std::make_tuple(conflict_table.at(x).size(), x->get_shape().bytes()) <
+                   std::make_tuple(conflict_table.at(y).size(), y->get_shape().bytes());
+        });
+        // Process the conflict_queue, we refer to the current allocation as
+        // the parent and the adjacent allocations as children
+        for(auto parent : conflict_queue)
+        {
+            // Sort children by size
+            std::vector<instruction_ref> children(conflict_table.at(parent).begin(),
+                                                  conflict_table.at(parent).end());
+            std::sort(children.begin(), children.end(), [](auto x, auto y) {
+                return x->get_shape().bytes() < y->get_shape().bytes();
+            });
+            // This set is to track the segments already processed
+            std::set<segment> segments;
+            // Add all segemnts for the children to the segments already processed
+            transform_if(children.begin(),
+                           children.end(),
+                           std::inserter(segments, segments.begin()),
+                           [&](auto child) { return as.get_segment(child); }, 
+                           [&](auto child) { return *as.get_segment(child); });
+
+            // Get the segement for the parent
+            auto* parent_segment = as.get_segment(parent);
+            // Add segment for the parent if there is none or segment overlaps with the children
+            if (parent_segment == nullptr or overlaps(segments, *parent_segment))
+                as.add_segment(parent, next_segment(segments, parent));
+            else
+                segments.insert(*parent_segment);
+
+            for(auto child : children)
+            {
+                assert(child != parent);
+                if (not as.get_segment(child))
+                    as.add_segment(child, next_segment(segments, child));
+            }
+        }
+        return as;
+    }
+};
 
 // A class to manage allocation colors
 struct allocation_color
@@ -272,47 +400,42 @@ struct allocation_color
 void memory_coloring::apply(module& m) const
 {
     auto conflict_table = build_conflict_table(m, allocation_op);
-    auto ac             = allocation_color::build(conflict_table);
+    auto as             = allocation_segment::build(conflict_table);
 
-    // All allocations should have a color
+    // All allocations should have a segment
     assert(std::all_of(conflict_table.begin(), conflict_table.end(), [&](auto&& pp) {
-        return ac.get_color(pp.first) >= 0;
+        return as.get_segment(pp.first);
     }));
 
-    // Adjacent allocations should not share the same color
+    // Adjacent allocations should not have overlapping segments 
     assert(std::none_of(conflict_table.begin(), conflict_table.end(), [&](auto&& pp) {
-        auto c = ac.get_color(pp.first);
+        auto* x = as.get_segment(pp.first);
         return std::any_of(
-            pp.second.begin(), pp.second.end(), [&](auto ins) { return ac.get_color(ins) == c; });
+            pp.second.begin(), pp.second.end(), [&](auto ins) {
+                auto* y = as.get_segment(ins);
+                assert(x and y);
+                return is_overlap(*x, *y);
+            });
     }));
 
-    const std::size_t alignment = 32;
     // Total memory
     std::size_t n = 0;
-    // Compute offsets
-    std::map<int, int> color2offset;
-    for(auto&& pp : ac.color2ins)
+    for(auto&& pp : as.ins2segment)
     {
-        auto color = pp.first;
-        color2offset.emplace(color, n);
-        std::size_t size    = ac.max_bytes(color);
-        std::size_t padding = (alignment - (size % alignment)) % alignment;
-        n += size + padding;
+        auto seg = pp.second;
+        n = std::max(n, seg.second*alignment);
     }
 
     // Replace allocations
     auto mem = m.add_parameter("scratch", shape{shape::int8_type, {n}});
-    for(auto&& pp : ac.color2ins)
+    for(auto&& pp : as.ins2segment)
     {
-        auto color         = pp.first;
-        auto&& allocations = pp.second;
-        auto offset        = color2offset.at(color);
-        for(auto ins : allocations)
-        {
-            assert(ins->name() == allocation_op);
-            auto s = ins->get_shape();
-            m.replace_instruction(ins, op::load{s, std::size_t(offset)}, mem);
-        }
+        auto ins = pp.first;
+        auto seg = pp.second;
+        assert(ins->name() == allocation_op);
+        auto s = ins->get_shape();
+        std::size_t offset = seg.first*32;
+        m.replace_instruction(ins, op::load{s, offset}, mem);
     }
 }
 
