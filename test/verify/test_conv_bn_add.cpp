133e5f1be34219286320201019f9174c5cb2f0cc
Khalique <15948690+kahmed10@users.noreply.github.com>
2021-01-11 12:36:18 -0600

    formatting

--- test/verify/test_conv_bn_add.cpp
+++ test/verify/test_conv_bn_add.cpp
@@ -13,14 +13,13 @@ struct test_conv_bn_add : verify_program<test_conv_bn_add>
                                             std::size_t seed = 1)
     {
         migraphx::shape vars{migraphx::shape::float_type, {channels}};
-        auto* mm = p.get_main_module();
-        auto scale    = mm->add_literal(migraphx::abs(migraphx::generate_literal(vars, 1 +
-        seed))); auto bias     = mm->add_literal(migraphx::abs(migraphx::generate_literal(vars, 2
-        + seed))); auto mean     = mm->add_literal(migraphx::abs(migraphx::generate_literal(vars,
-        3 + seed))); auto variance =
-        mm->add_literal(migraphx::abs(migraphx::generate_literal(vars, 4 + seed))); 
-        return
-        mm->add_instruction(migraphx::make_op("batch_norm_inference"), x, scale, bias, mean, variance);
+        auto* mm      = p.get_main_module();
+        auto scale    = mm->add_literal(migraphx::abs(migraphx::generate_literal(vars, 1 + seed)));
+        auto bias     = mm->add_literal(migraphx::abs(migraphx::generate_literal(vars, 2 + seed)));
+        auto mean     = mm->add_literal(migraphx::abs(migraphx::generate_literal(vars, 3 + seed)));
+        auto variance = mm->add_literal(migraphx::abs(migraphx::generate_literal(vars, 4 + seed)));
+        return mm->add_instruction(
+            migraphx::make_op("batch_norm_inference"), x, scale, bias, mean, variance);
     }
 
     migraphx::program create_program() const
@@ -28,14 +27,14 @@ struct test_conv_bn_add : verify_program<test_conv_bn_add>
         migraphx::program p;
         std::size_t ichannels = 64;
         std::size_t ochannels = 256;
-        auto* mm = p.get_main_module();
-        auto x     = mm->add_parameter("x", {migraphx::shape::float_type, {1, ichannels, 56,
-        56}}); auto w     = mm->add_literal(migraphx::generate_literal(
+        auto* mm              = p.get_main_module();
+        auto x     = mm->add_parameter("x", {migraphx::shape::float_type, {1, ichannels, 56, 56}});
+        auto w     = mm->add_literal(migraphx::generate_literal(
             {migraphx::shape::float_type, {ochannels, ichannels, 1, 1}}, 1));
-        auto y     = mm->add_parameter("y", {migraphx::shape::float_type, {1, ichannels, 56,
-        56}}); auto v     = mm->add_literal(migraphx::generate_literal(
+        auto y     = mm->add_parameter("y", {migraphx::shape::float_type, {1, ichannels, 56, 56}});
+        auto v     = mm->add_literal(migraphx::generate_literal(
             {migraphx::shape::float_type, {ochannels, ichannels, 1, 1}}, 2));
-        auto relu1 = mm->add_instruction(migraphx::make_op("relu"),  x);
+        auto relu1 = mm->add_instruction(migraphx::make_op("relu"), x);
         auto conv1 = mm->add_instruction(migraphx::make_op("convolution"), relu1, w);
         auto bn1   = add_bn(p, conv1, ochannels, 1);
         auto relu2 = mm->add_instruction(migraphx::make_op("relu"), y);
